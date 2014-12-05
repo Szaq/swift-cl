53dd0ae8783017397bdd9bea28257f69f14e93d2
Łukasz Kwoska <lukasz.kwoska@firma.gg.pl>
2014-12-05 08:34:05 +0100

    Initial Commit

---
+++ SwiftCL/SwiftCL.h
@@ -0,0 +1,19 @@
+//
+//  SwiftCL.h
+//  SwiftCL
+//
+//  Created by Lukasz Kwoska on 05/12/14.
+//  Copyright (c) 2014 Spinal Development. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftCL.
+FOUNDATION_EXPORT double SwiftCLVersionNumber;
+
+//! Project version string for SwiftCL.
+FOUNDATION_EXPORT const unsigned char SwiftCLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftCL/PublicHeader.h>
+
+
